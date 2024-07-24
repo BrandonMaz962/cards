153634f365b5f0434cdd3be7600079239a14b199
Brandon Mazurkiewicz <boomerbash962@gmail.com>
2024-07-24 18:21:59 -0400

    changed to arraylists to add live update deck and avoid card duplication

--- cards/cards.java
+++ cards/cards.java
@@ -12,23 +12,35 @@ class cards {
 		int pick = 0;
 		int again = 0;
 		int handCards = 0;
+		int size = 52;
+
+		List<String> deck1 = new ArrayList<>();
+		List<String> hand1 = new ArrayList<>();
+		hand1.add("-");
+		hand1.add("-");
+		hand1.add("-");
+		hand1.add("-");
+		hand1.add("-");
 
 		for(int i = 0; i < suits.length; i++){
 			for(int j = 0; j < values.length; j++){
 				deck[count] = ((values[j] + suits[i] + " "));
+				deck1.add((values[j] + suits[i] + " "));
 				count ++;
 			}
 		}
 		
 		/*for(int i = 0; i < deck.length; i++){
 			System.out.print(deck[i] + " ");
-		}*/
+		}
+
+		System.out.println(deck1);*/
 
 		Random rand = new Random();
 		Scanner keyboard = new Scanner(System.in);
 
 		System.out.println("Deck of cards simulator.\n");
-		System.out.println("There are " + count + " cards in the deck.");
+		System.out.println("There are " + deck1.size() + " cards in the deck.");
 
 		do{
 		System.out.print("\nWhat would you like to do: \n");
@@ -37,40 +49,44 @@ class cards {
 		int choice = keyboard.nextInt();
 
 		if(choice == 1){
-			pick = rand.nextInt(52);
-			System.out.print("\nYou drew a: " + deck[pick] + "\n");
+
+			if(deck1.size() > 0){
+				pick = rand.nextInt(size);
+				System.out.print("\nYou drew a: " + deck1.get(pick) + "\n");
 			
-			if(handCards < 5){
-				hand[handCards] = deck[pick];
-				handCards ++;
-				count -= 1;
+				if(handCards < 5){
+					hand1.set(handCards, deck1.get(pick));
+					handCards ++;
+					deck1.remove(pick);
+					size -= 1;
+				}else{
+					System.out.println("Hand is currently full! Need to discard before drawing again.\n");
+				}
 			}else{
-				System.out.println("Hand is currently full! Need to discard before drawing again.\n");
+				System.out.println("The deck is currently empty!\n");
 			}
-			
 		}else if(choice ==2){
 
 			if(handCards > 0) {
 				System.out.print("Which card would you like to discard (1,2,3,4,5): ");
 				int temp = keyboard.nextInt();
-				hand[temp] = "-";
+				deck1.add(hand1.get(temp - 1));
+				hand1.set((temp - 1), "-");
 				handCards -= 1;
-				count += 1;
+				size += 1;
 			}else{
 				System.out.print("\nYou have no cards in your hand! Need to draw before you can discard.\n");
 			}
 
 		}else if(choice == 3){
-			System.out.print("Your current hand: ");
-			for(int i = 0; i < 5; i++){
-				System.out.print(hand[i] + " ");
-			}
-		}else{
+			System.out.print("\nYour current hand: ");
+			System.out.print(hand1);
+		}else {
 			again = 1;
 			System.out.print("Goodbye\n\n");	
 		}
 		
-		System.out.print("\nThere are now " + count + " cards left in the deck\n");
+		System.out.print("\nThere are now " + deck1.size() + " cards left in the deck\n");
 		
 		}while(again == 0);
 	}
